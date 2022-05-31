9fc5b863cbc22d30898944bd4c787667ff901b28
Eugene <69599396+Eugene-beginning-of-the-path@users.noreply.github.com>
2022-05-31 11:54:25 +0300

    Added 2 apps for communication Server-Client

---
+++ TestClientSocket/TestClientSocket.cpp
@@ -0,0 +1,114 @@
+﻿#define WIN32_LEAN_AND_MEAN
+
+#include <iostream>
+#include <windows.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "Client is starting..." << endl;
+	WSADATA wsadata;
+	if (WSAStartup(MAKEWORD(2, 2), &wsadata) != 0) //открыли интерфесы для работы с сокетами windows 
+	{
+		cout << "Error in initialization of WinSock interface" << endl;
+		return 1;
+	}
+
+	ADDRINFO hints; //выставляем параметры для поиска сокета на стороне сервера 
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET; //IPv4
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+
+	ADDRINFO* addrResult;
+	//ищем на стороне сервера сокет, с нужными нам параметрами, переданными через hints
+	//addrResult получит сокет(-ы), удовлетворяющие нашему запросу 
+	int errorStat = getaddrinfo("localhost", "666", &hints, &addrResult);
+	if (errorStat != 0)
+	{
+		cout << "getaddrinfo failed with error" << endl;
+		WSACleanup();
+		return 1;
+	}
+
+	SOCKET toServerSocket = INVALID_SOCKET; //создаем сокет и временно его закрываем 
+	toServerSocket = socket(addrResult->ai_family, addrResult->ai_socktype, addrResult->ai_protocol); //создаем сокет на базе найденного Сервер-сокета addrResult
+	if (toServerSocket == INVALID_SOCKET)
+	{
+		cout << "Socket creation failled" << endl;
+		WSACleanup();
+		freeaddrinfo(addrResult);
+		return 1;
+	}
+
+	errorStat = connect(toServerSocket, addrResult->ai_addr, (int)addrResult->ai_addrlen);
+	if (errorStat == SOCKET_ERROR)
+	{
+		cout << "Unable connect to server";
+		closesocket(toServerSocket);
+		toServerSocket = INVALID_SOCKET;
+
+		freeaddrinfo(addrResult);
+		WSACleanup();
+		return 1;
+	}
+
+	const char* sendBuffer = "Hello from Client";
+	errorStat = send(toServerSocket, sendBuffer, (int)strlen(sendBuffer), 0); //send() возвращает кол-во переданных данных/SOCKET_ERROR
+	if (errorStat == SOCKET_ERROR)
+	{
+		cout << "Unable send a message to Server";
+		closesocket(toServerSocket);
+		toServerSocket = INVALID_SOCKET;
+
+		freeaddrinfo(addrResult);
+		WSACleanup();
+		return 1;
+	}
+
+	cout << "Bytes sent to Server " << errorStat << " bytes with the message - " << sendBuffer << " (1)" << endl; //выводим размер отправленного сообщения
+
+	errorStat = shutdown(toServerSocket, SD_SEND);
+	//SD_SEND - передача данных через сокет прекращена, а прием на сокет продолжает работать - отключили сокет от передачи данных
+	if (errorStat == SOCKET_ERROR)
+	{
+		cout << "Shutdown error";
+		closesocket(toServerSocket);
+		toServerSocket = INVALID_SOCKET;
+
+		freeaddrinfo(addrResult);
+		WSACleanup();
+		return 1;
+	}
+
+	char recvBuffer[512];
+	//т.к. сокет теперь работает в режиме только прослушки (не отправляет инфу на сервер), мы лишь бесконечно получаем инфу с сервера, пока цикл не прервётся
+	do
+	{
+		ZeroMemory(recvBuffer, sizeof(recvBuffer));
+		errorStat = recv(toServerSocket, recvBuffer, 512, 0);
+		if (errorStat > 0)
+		{
+			cout << "Received a message from Server (4):" << endl;
+			cout << "Received " << errorStat << " bytes" << endl;
+			cout << "Received data: " << recvBuffer << endl;
+		}
+		else if (errorStat == 0)
+			cout << "Connection closed" << endl;
+		else
+			cout << "recv failed" << endl;
+	} while (errorStat > 0);
+
+	closesocket(toServerSocket);
+	toServerSocket = INVALID_SOCKET;
+
+	freeaddrinfo(addrResult);
+	WSACleanup();
+	return 0;
+
+}
